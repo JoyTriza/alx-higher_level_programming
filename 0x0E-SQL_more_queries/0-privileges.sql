cd9c9a20b5ac909d419ae69731e570273b9ba189
JoyTriza <wangarijoytriza@gmail.com>
2023-08-16 04:22:07 -0700

    Add completed

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Lists all privileges of the users user_1 and user_2
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Lists all cities of CA in the database hbtn_usa
+-- Results are ordered by ascending cities.id
+
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
